6fb87a6623a1b49e38172e4d00ba59f0d3b4f25a
Alatr <dimalukianovweb@gmail.com>
2019-11-28 15:42:48 +0200

    table

--- src/containers/tickets/tickets.js
+++ src/containers/tickets/tickets.js
@@ -1,12 +1,47 @@
 import React from 'react';
 import withStore from '~/hocs/withStore';
 
+import styles from '~/containers/tickets/css/ticket.module.css';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
+
 class Tickets extends React.Component{
    render(){
-      let ticketsStore = this.props.stores.tickets;
-      console.log(ticketsStore.ticketsList);
-      
-      return <h1>hi, bro this is Tickets component</h1>
+		let ticketsStore = this.props.stores.tickets;
+		 return (
+			 <Paper className={styles.root}>
+				 <div className={styles.tableWrapper}>
+					 <Table stickyHeader aria-label="sticky table">
+						 <TableHead>
+							 <TableRow>
+								 {ticketsStore.tableCell.map(column=> (
+									 <TableCell key={column.id} >
+										 {column.label}
+									 </TableCell>
+								 ))}
+							 </TableRow>
+						 </TableHead>
+						 <TableBody>
+							 {ticketsStore.ticketsList.map((row, i) => {
+								 return  <TableRow key={i}>
+														{ticketsStore.tableCell.map(column => {
+															const value = row[column.key];
+															return <TableCell key={column.id} >
+																				{column.format && typeof value === 'number' ? column.format(value, 3) : column.format(value)}
+																			</TableCell>
+														})}
+													</TableRow>
+							 })}
+						 </TableBody>
+					 </Table>
+				 </div>
+			 </Paper>
+		 );
    }
 }
 
--- src/store/tickets-store.js
+++ src/store/tickets-store.js
@@ -5,19 +5,105 @@ export default class Tickets{
 		this.rootStore = rootStore;
     this.api = this.rootStore.api.tickets;
 	}
-
 	@observable ticketsList = [];
+	@observable tableCell = [
+		{
+				key: 'actual',
+				label: 'Актуальность',
+				id: 'id_actual,',
+				format: this.formatRule.bool
+		},
+		{
+				key: 'depart_date',
+				label: 'дата отправления',
+				id: 'id_depart_date,',
+				format: this.formatRule.string
+		},
+		{
+				key: 'destination',
+				label: 'пункт назначения',
+				id: 'id_destination,',
+				format: this.formatRule.string
+		},
+		{
+				key: 'origin',
+				label: 'пункт отправления',
+				id: 'id_origin,',
+				format: this.formatRule.string
+		},
+		{
+				key: 'return_date',
+				label: 'дата возвращения',
+				id: 'id_return_date,',
+				format: this.formatRule.string
+		},
+		{
+				key: 'value',
+				label: 'Цена',
+				id: 'id_value,',
+				format: this.formatRule.number
+		},
+		{
+				key: 'number_of_changes',
+				label: 'количество пересадок',
+				id: 'id_number_of_changes,',
+				format: this.formatRule.number
+		},
+		{
+				key: 'found_at',
+				label: 'когда был найден билет',
+				id: 'id_found_at,',
+				format: this.formatRule.string
+		},
+	];
+
+	get formatRule() {
+		return {
+			bool: (value) => {
+				if (value != null) {
+					return value ? 'Да' : 'Нет'
+				}
+			},
+			string: (value) => {
+				if (value != null) {
+					return value.toLocaleString();
+				}
+			},
+			number: (value, num = 2) => {
+				if (value != null) {
+					return value.toFixed(num);
+				}
+			}
+		}
+	}
+
+	@computed get tableCellKey() {
+		return this.tableCell.map(el => el.key)
+	}
+
+	@action changeTicketsListAPI(obj){
+		obj.forEach((el)=>{
+			let item = {}
+			for (const key in el) {
+				if (el.hasOwnProperty(key) && this.tableCellKey.includes(key)) {
+					item[key] = el[key];
+				}
+			}
+			this.ticketsList.push(item)
+		});
+	}
 
 
 	@action load(){
 		return new Promise((resolve)=>{
-
 			this.api.load().then((data)=>{
 				runInAction(() => {
-					this.ticketsList = data;
+					console.log(data.data);
+					
+					this.changeTicketsListAPI(data.data);
 					resolve();
 				});
-			})
-		})
+			});
+		});
 	}
 }
